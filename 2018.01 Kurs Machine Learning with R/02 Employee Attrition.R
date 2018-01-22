52d70d03044b0c3fcb9d9539e236b8f83c6e6446
Samuel Reuther <samuel.reuther@baloise.ch>
2018-01-22 16:04:02 +0100

    ...

--- 2018.01 Kurs Machine Learning with R/02 Employee Attrition.R
+++ 2018.01 Kurs Machine Learning with R/02 Employee Attrition.R
@@ -15,7 +15,7 @@ options(repos = c(CRAN = "https://mran.revolutionanalytics.com/snapshot/2018-01-
 
 # load some libraries
 library(pacman)
-p_load(dplyr, ggplot2, gridExtra, caret, reshape2, Metrics) 
+p_load(dplyr, ggplot2, gridExtra, caret, reshape2, Metrics, plotmo) 
 
 # source some additional functions
 source("00 Functions.R")
@@ -43,7 +43,7 @@ summary(df)
 df$Attrition <- ifelse(df$Attrition == "No", 0, 1)
 
 # Remove columns with only 1 value
-df <- Remove_columns_with_only_one_value(df)
+df <- remove_columns_with_only_one_value(df)
 
 
 
@@ -81,9 +81,9 @@ test <- df %>%
 #                             
 #                          untuned     tuned     Comment
 # 1.  mean ensemble         0.???      0.???     
-# 2.  XGBOOST               0.???      0.???     
-# 3.  GBM                   0.???      0.???     
-# 4.  randomforrest         0.855      0.???     
+# 2.  XGBOOST               0.853      0.???     
+# 3.  GBM                   0.867      0.???     
+# 4.  randomforrest         0.855      0.867     
 # 5.  mars                  0.878      0.878     GLM
 # 7.  base line             0.830                Most frequent value as prediction
 
@@ -154,11 +154,11 @@ rm(mars, pr, ev)
 p_load(randomForest)
 
 # train model (mtry at default value = floor(sqrt(ncol(train))) = 5)
-rf <- randomForest(as.factor(Attrition)~., data = train, ntree = 1000, importance = T)
+rf <- randomForest(as.factor(Attrition)~., data = train, ntree = 5000, importance = T)
 rf
 
 # train model with specific mtry
-rf <- randomForest(Attrition~., data = train, ntree = 1000, mtry = 10, importance = T)
+rf <- randomForest(as.factor(Attrition)~., data = train, ntree = 5000, mtry = 13, importance = T)
 rf
 
 # show confusion matrix
@@ -171,8 +171,7 @@ plot(rf)
 # plot importance of each variable
 varImpPlot(rf, sort = T, scale = T)
 
-# Partial Dependence Plot
-p_load(plotmo)
+# partial dependence plot
 plotmo(rf, nrug = "density", pmethod = "apartdep", ylim = NULL, degree2 = F)
 
 
@@ -182,7 +181,7 @@ tuneRF(train %>% select(-Attrition), as.factor(train$Attrition),
        ntreeTry = 5000, mtryStart = 10, stepFactor = 1.3, improve = 0.00001, 
        trace = T, plot = T, doBest = F)
 
-# Schleife, um den besten Parameter mtry zu finden
+# Parametertuning: mtry über eine Schleife optimieren
 Summary <- data.frame(mtry = c(2, 3, 4, 5, 6, 8, 10, 13, 16, 20, 25, 30), train = NA, test = NA)
 i <- 1
 for (mtry in Summary$mtry) {
@@ -196,18 +195,155 @@ for (mtry in Summary$mtry) {
   rm(rf)
 }
 print(Sys.time())
+# show results
 Summary
-ggplot(data = melt(Summary, id = "mtry"), aes(x = mtry, y = value, colour = variable)) + 
+ggplot(melt(Summary, id = "mtry"), aes(x = mtry, y = value, colour = variable)) + 
   geom_line()
 print(paste0("Maximum accuracy at mtry = ", Summary$mtry[which.max(Summary$test)]))
 
 
 
 # clean up
-rm(rf, Summary, mtry, i)
+rm(rf, Summary, mtry, i, pr)
 
 
 
 
 # GBM ---------------------------------------------------------------------
 
+# load library
+p_load(gbm)
+
+# train model
+ntrees <- 500
+gbm <- gbm(Attrition~., data = train, distribution = "bernoulli",
+           n.trees = ntrees, shrinkage = 0.05, 
+           interaction.depth = 3,     # 1: additive model, 2: two-way interactions, etc.
+           n.minobsinnode = 10,
+           bag.fraction = 0.5,
+           cv.folds = 5)
+gbm
+
+# plot how error developes during training process
+best_ntrees <- gbm.perf(gbm, method = "cv")
+
+# show confusion matrix
+pr <- predict(gbm, test %>% select(-Attrition), n.trees = best_ntrees)
+confusionMatrix(ifelse(pr > 0, 1, 0), as.factor(test$Attrition))
+
+# variable importance
+VarImp <- summary(gbm, n.trees = best_ntrees)
+VarImp
+
+# Partial dependence plot
+temp_names <- VarImp[1:4, ]
+temp_names <- as.character(temp_names$var[order(-temp_names$rel.inf)])
+par(mfrow = c(2, 2))
+for (i in 1:length(temp_names)) {
+  plot(gbm, i.var = temp_names[i], ntrees = best_ntrees, type = "link") 
+}
+par(mfrow = c(1, 1))
+
+# 2 way-interactions
+# calculate most important interactions
+zaehler <- 1
+tm <- names(train %>% select(-Attrition))
+ie <- data.frame(matrix(NA, nrow = length(tm) * (length(tm) - 1) / 2, ncol = 3))
+names(ie) <- c("Var1", "Var2", "H_Statistic")
+for (i in 1:(length(tm) - 1)) {
+  for (j in (i + 1):length(tm)) {
+    ie[zaehler, 1] <- tm[i]
+    ie[zaehler, 2] <- tm[j]
+    ie[zaehler, 3] <- round(interact.gbm(gbm, df, i.var = c(i, j), n.trees = best_ntrees), 5)
+    zaehler <- zaehler + 1
+  } 
+}
+ie <- ie %>% arrange(-H_Statistic)
+# plot most important interactions
+top <- 5
+ie_top <- ie[1:top, ]
+ie_top
+for (i in top:1) { 
+  plot(gbm, i.var = c(ie$Var1[i], ie$Var2[i]), n.trees = best_ntrees, return.grid = F)
+}
+
+# 3 way-interactions (slow!)
+# calculate most important interactions
+zaehler <- 1
+tm <- names(train %>% select(-Attrition))
+ie <- data.frame(matrix(NA, nrow = length(tm) * (length(tm) - 1) * (length(tm) - 2) / 6, ncol = 4))
+names(ie) <- c("Var1", "Var2", "Var3", "H_Statistic")
+for (i in 1:(length(tm) - 2)) {
+  for (j in (i + 1):(length(tm) - 1)) {
+    for (k in (j + 1):length(tm)) {
+      ie[zaehler, 1] <- tm[i]
+      ie[zaehler, 2] <- tm[j]
+      ie[zaehler, 3] <- tm[k]
+      ie[zaehler,4] <- round(interact.gbm(gbm, df, i.var = c(i, j, k), n.trees = best_ntrees), 8)
+      zaehler <- zaehler + 1
+    } 
+  }
+}
+ie <- ie %>% arrange(-H_Statistic)
+# plot most important interactions
+top <- 5
+ie_top <- ie[1:top, ]
+ie_top
+for (i in top:1) {
+  plot(gbm, i.var = c(ie$Var1[i], ie$Var2[i], ie$Var3[i]), n.trees = best_ntrees, return.grid = F)
+}
+
+
+
+# clean up
+rm(gbm, ie, ie_top, top, VarImp, best_ntrees, ntrees, i, j, k, pr, temp_names, 
+   temp_names2, tm, zaehler)
+
+
+
+
+# XGBOOST -----------------------------------------------------------------
+
+# load library
+p_load(xgboost, Matrix)
+
+# prepare data
+train_y <- train$Attrition
+train_mat <- sparse.model.matrix(~., data = train %>% select(-Attrition))
+train_d <- xgb.DMatrix(data = train_mat, label = train_y)
+test_y <- test$Attrition
+test_mat <- sparse.model.matrix(~., data = test %>% select(-Attrition))
+test_d <- xgb.DMatrix(data = test_mat, label = test_y)
+
+# train model
+xgb <- xgb.cv(data = train_d, 
+              objective = "binary:logistic", eval_metric = "error", nfold = 5,
+              nrounds = 1000, eta = 0.05,
+              max_depth = 6, min_child_weight = 1, gamma = 0,
+              subsample = 1, colsample_bytree = 1,
+              early_stopping_rounds = 30)
+
+# plot how error developes during training process
+ggplot(melt(xgb$evaluation_log %>% select(-train_error_std, -test_error_std), id = "iter"),
+       aes(x = iter, y = value, colour = variable)) +
+  geom_line(size = I(1))
+
+# re-run model with nrounds = best_nrounds from cv
+xgb <- xgb.train(data = train_d, 
+                 objective = "binary:logistic", eval_metric = "error",
+                 nrounds = 57, eta = 0.05,
+                 max_depth = 6, min_child_weight = 1, gamma = 0,
+                 subsample = 1, colsample_bytree = 1)
+
+# show confusion matrix
+pr <- predict(xgb, test_d)
+confusionMatrix(ifelse(pr > 0.5, 1, 0), test$Attrition)
+
+# variable importance
+xgb.importance(colnames(train_mat), xgb)
+xgb.plot.importance(xgb.importance(colnames(train_mat), xgb), rel_to_first = T, xlab = "Relative importance")
+
+
+
+# clean up
+rm(train_y, train_mat, train_d, test_y, test_mat, test_d, xgb, pr)
